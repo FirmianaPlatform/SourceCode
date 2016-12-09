b6cfcdf5cac94d2bcef1fcef67bb42c17012444d
Firmiana Platform <admin@firmiana.org>
2016-12-09 09:27:54 +0800

    Update models.py

--- Firmiana Frontend/repository/models.py
+++ Firmiana Frontend/repository/models.py
@@ -6,6 +6,7 @@ from experiments.models import AuthParentChildAssociation
 
 ''' MIAPE Metadata '''
 class Miape_ExpType(models.Model):
+    '''Experimental type complying with MIAPE standard'''
     name = models.CharField(max_length=255, unique=True)
     validated = models.BooleanField(default=False)
 
@@ -13,6 +14,7 @@ class Miape_ExpType(models.Model):
         return self.name
         
 class Miape_Species(models.Model):
+    '''Species complying with MIAPE standard'''
     name = models.CharField(max_length=255, unique=True)
     validated = models.BooleanField(default=True)
 
@@ -20,6 +22,7 @@ class Miape_Species(models.Model):
         return self.name
         
 class Miape_Tissue(models.Model):
+    '''Tissues complying with MIAPE standard'''
     name = models.CharField(max_length=255, unique=True)
     validated = models.BooleanField(default=True)
 
@@ -27,6 +30,7 @@ class Miape_Tissue(models.Model):
         return self.name
         
 class Miape_Modification(models.Model):
+    '''Modifications complying with MIAPE standard'''
     name = models.CharField(max_length=255, unique=True)
     validated = models.BooleanField(default=True)
 
@@ -34,6 +38,7 @@ class Miape_Modification(models.Model):
         return self.name
         
 class Miape_Instrument(models.Model):
+    '''Instrument name complying with MIAPE standard'''
     name = models.CharField(max_length=255, unique=True)
     validated = models.BooleanField(default=True)
 
@@ -41,6 +46,7 @@ class Miape_Instrument(models.Model):
         return self.name
         
 class Miape_CellType(models.Model):
+    '''Cell type complying with MIAPE standard'''
     name = models.CharField(max_length=255, unique=True)
     validated = models.BooleanField(default=True)
 
@@ -48,6 +54,7 @@ class Miape_CellType(models.Model):
         return self.name
         
 class Miape_Disease(models.Model):
+    '''Disease name complying with MIAPE standard'''
     name = models.CharField(max_length=255, unique=True)
     validated = models.BooleanField(default=True)
 
@@ -55,6 +62,7 @@ class Miape_Disease(models.Model):
         return self.name
         
 class Miape_QuantMethod(models.Model):
+    '''Quantification method complying with MIAPE standard'''
     name = models.CharField(max_length=255, unique=True)
     validated = models.BooleanField(default=True)
 
@@ -63,12 +71,14 @@ class Miape_QuantMethod(models.Model):
 
 ''' Project '''
 class ProjectStatus(models.Model):
+    '''Status of project in data repository of Fimiana'''
     status = models.CharField(max_length=90, unique=True)
     validated = models.BooleanField(default=True)
     def __unicode__(self):
         return self.status
 
 class ProjectDetail(models.Model):
+    '''Detail information of a project in data repository of Fimiana'''
     pxdNo = models.CharField(max_length=255, unique=True)
     projectName = models.TextField()
     keywords = models.TextField()
@@ -98,6 +108,7 @@ class ProjectDetail(models.Model):
 
     
 class ProjectOverview(models.Model):
+    '''Overview of a project in data repository of Fimiana'''
     id = models.OneToOneField(ProjectDetail, db_column='id', primary_key=True)
     pxdNo = models.CharField(max_length=255, unique=True)
     status = models.ForeignKey(ProjectStatus, unique=False)
@@ -109,6 +120,7 @@ class ProjectOverview(models.Model):
          return self.pxdNo
 
 class ChildUserAndSharedProject(models.Model):
+    '''The assotiation between sub-account and shared projects'''
     child = models.ForeignKey(AuthChildUser)
     sharedProject = models.TextField()
     isActive = models.BooleanField(default=True)
@@ -116,6 +128,7 @@ class ChildUserAndSharedProject(models.Model):
         return self.sharedProject
      
 class PublicatedProject(models.Model):
+    '''The information of publicated project'''
     pxdNo = models.CharField(max_length=255, unique=True)
     oldOwner = models.CharField(max_length=255, unique=False)
     newOwner = models.CharField(max_length=255, unique=False, default="Guest")
